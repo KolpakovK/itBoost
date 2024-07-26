7ad718d3ebcb343edcba84ba04187b2c7f80a7fb
kyrylo <kyrylo@buckhill.co.uk>
2024-07-26 19:16:38 +0200

    First commit

---
+++ src/components/login/login_form.tsx
@@ -0,0 +1,91 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { toast } from "@/components/ui/use-toast"
+
+const FormSchema = z.object({
+  username: z.string().min(5, {
+    message: "Ім'я користувача не може бути менше ніж 5 символів.",
+  }),
+  password: z.string()
+})
+
+export function InputForm() {
+  const form = useForm<z.infer<typeof FormSchema>>({
+    resolver: zodResolver(FormSchema),
+    defaultValues: {
+      username: "",
+      password: ""
+    },
+    require:{
+        username: true,
+        password: true,
+    }
+  })
+
+  function onSubmit(data: z.infer<typeof FormSchema>) {
+    toast({
+      title: "You submitted the following values:",
+      description: (
+        <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
+          <code className="text-white">{JSON.stringify(data, null, 2)}</code>
+        </pre>
+      ),
+    })
+  }
+
+  return (
+    <div className="lg:w-2/3 flex flex-col gap-8">
+
+        <div className="flex flex-col gap-0">
+            <p className=" text-gray-500 text-lg font-light">Увійдіть до кабінету щоб розпочати роботу</p>
+            <h1 className=" text-gray-950 text-[64px] font-bold leading-tight font-display">Навчальна платформа <span className="text-violet-500">ITBoost</span></h1> 
+        </div>
+
+        <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+                <div className="space-y-4">
+                    <FormField control={form.control} name="username" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Логін</FormLabel>
+                            <FormControl>
+                                <Input {...field} />
+                            </FormControl>
+                            <FormDescription>
+                                Використовуйте логін котрий вам дала адміністрація закладу
+                            </FormDescription>
+                            <FormMessage />
+                        </FormItem>
+                    )}/>
+
+                    <FormField control={form.control} name="password" render={({ field }) => (
+                        <FormItem>
+                            <FormLabel>Пароль</FormLabel>
+                            <FormControl>
+                                <Input type="password" {...field} />
+                            </FormControl>
+                            <FormMessage />
+                        </FormItem>
+                    )}/>
+                </div>
+
+                <Button type="submit" className="w-full">Увійти</Button>
+            </form>
+        </Form>
+    </div>
+  )
+}
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,20 +1,75 @@
-import type { Config } from "tailwindcss";
+import type { Config } from "tailwindcss"
 
-const config: Config = {
-  content: [
-    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
-  ],
-  theme: {
-    extend: {
-      backgroundImage: {
-        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
-        "gradient-conic":
-          "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
-      },
+const config = {
+    darkMode: ["class"],
+    content: [
+        './pages/**/*.{ts,tsx}',
+        './components/**/*.{ts,tsx}',
+        './app/**/*.{ts,tsx}',
+        './src/**/*.{ts,tsx}',
+    ],
+    prefix: "",
+    theme: {
+        container: {
+            center: true,
+            padding: "2rem",
+            screens: {
+                "2xl": "1400px",
+            },
+        },
+        fontFamily:{
+            'display':['Dela Gothic One','ui-sans-serif', 'system-ui'],
+            'sans': ['IBM Plex Sans','ui-sans-serif', 'system-ui'],
+            'serif': ['ui-serif', 'Georgia'],
+            'mono': ['ui-monospace', 'SFMono-Regular'],
+        },
+        extend: {
+            keyframes: {
+                "accordion-down": {
+                    from: { height: "0" },
+                    to: { height: "var(--radix-accordion-content-height)" },
+                },
+                "accordion-up": {
+                    from: { height: "var(--radix-accordion-content-height)" },
+                    to: { height: "0" },
+                },
+            },
+            animation: {
+                "accordion-down": "accordion-down 0.2s ease-out",
+                "accordion-up": "accordion-up 0.2s ease-out",
+            },
+
+            colors: {
+                'green': {
+                    '50': '#f1fce9',
+                    '100': '#e0f8cf',
+                    '200': '#c3f1a5',
+                    '300': '#9ce670',
+                    '400': '#84da53',
+                    '500': '#5abc26',
+                    '600': '#43961a',
+                    '700': '#347318',
+                    '800': '#2d5b19',
+                    '900': '#284e19',
+                    '950': '#112a09',
+                },
+                'violet': {
+                    '50': '#f3f2ff',
+                    '100': '#ebe7ff',
+                    '200': '#d9d3ff',
+                    '300': '#bcafff',
+                    '400': '#9c82ff',
+                    '500': '#7d4fff',
+                    '600': '#7130fc',
+                    '700': '#6019e8',
+                    '800': '#4f15c2',
+                    '900': '#43139f',
+                    '950': '#27096c',
+                },
+            }
+        },
     },
-  },
-  plugins: [],
-};
-export default config;
+    plugins: [require("tailwindcss-animate")],
+} satisfies Config
+
+export default config
